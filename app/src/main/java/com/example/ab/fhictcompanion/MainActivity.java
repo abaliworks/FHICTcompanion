2e0ac4691bace78077f94a44dbeec842d36dfed9
ab <shakawaka@protonmail.com>
2018-10-16 17:30:37 +0200

    add week 2 assignment

--- app/src/main/java/com/example/ab/fhictcompanion/MainActivity.java
+++ app/src/main/java/com/example/ab/fhictcompanion/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.ab.fhictcompanion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mClickMeButton;
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mClickMeButton = (Button) findViewById(R.id.clickMe_Button);
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        mClickMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setText("You clicked the button");
+            }
+        });
+        mClickMeButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                mTextView.setText("You Long clicked the button");
+                return true;
+            }
+        });
+
     }
 }
