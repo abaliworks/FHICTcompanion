69769eec37220354415c1db5e87a98ddf9eec275
ab <shakawaka@protonmail.com>
2018-10-17 17:28:27 +0200

    add authentication from Fontys API

---
+++ app/src/main/java/com/example/ab/fhictcompanion/StartActivity.java
@@ -0,0 +1,23 @@
+package com.example.ab.fhictcompanion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity implements TokenFragment.OnFragmentInteractionListener {
+    public static String token;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    @Override
+    public void onFragmentInteraction(String token) {
+        this.token =token;
+        Intent intent = new Intent(StartActivity.this, MainActivity.class);
+        intent.putExtra("token", token);
+        startActivity(intent);
+        finish();
+    }
+}
