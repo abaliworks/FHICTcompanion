7a94dbbeaf1390276d0a6b6b787164c96ea230f2
ab <shakawaka@protonmail.com>
2018-10-20 00:29:39 +0200

    add functional home page

--- app/src/main/java/com/example/ab/fhictcompanion/MainActivity.java
+++ app/src/main/java/com/example/ab/fhictcompanion/MainActivity.java
@@ -1,36 +1,151 @@
 package com.example.ab.fhictcompanion;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.example.ab.fhictcompanion.Model.Profile;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button mClickMeButton;
-    private TextView mTextView;
+    private Profile mProfile;
+    private ImageView mImageView;
+    private TextView mDisplayName, mFhictId, mStudentId, mClassTitle, mDepartment, mEmail;
+    private ProgressDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mClickMeButton = (Button) findViewById(R.id.clickMe_Button);
-        mTextView = (TextView) findViewById(R.id.textView);
+        mDialog = new ProgressDialog(MainActivity.this);
 
-        mClickMeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mTextView.setText("You clicked the button");
-            }
-        });
-        mClickMeButton.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                mTextView.setText("You Long clicked the button");
-                return true;
-            }
-        });
+        mImageView = (ImageView) findViewById(R.id.image);
+        mDisplayName = (TextView) findViewById(R.id.displayName);
+        mFhictId = (TextView) findViewById(R.id.fhictId);
+        mStudentId = (TextView) findViewById(R.id.studentId);
+        mClassTitle = (TextView) findViewById(R.id.classTitle);
+        mDepartment = (TextView) findViewById(R.id.department);
+        mEmail = (TextView) findViewById(R.id.email);
 
+        new JSONTask().execute();
+    }
+    public class JSONTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://api.fhict.nl/people/me");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setRequestProperty("Authorization", "Bearer " + StartActivity.token);
+                connection.connect();
+
+                InputStream is = new BufferedInputStream(connection.getInputStream());
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                StringBuilder sb = new StringBuilder();
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+                String response = sb.toString();
+
+                JSONObject json = new JSONObject(response);
+
+                String stringUrlPhoto = json.getString("photo");
+                String displayName = json.getString("displayName");
+                String fhictId = json.getString("id");
+                String studentId = json.getString("employeeId");
+                String classTitle = json.getString("personalTitle");
+                String department = json.getString("department");
+                String email = json.getString("mail");
+
+                URL urlPhoto = new URL(stringUrlPhoto);
+                HttpURLConnection connectionPhoto = (HttpURLConnection) urlPhoto.openConnection();
+                connectionPhoto.setRequestProperty("Accept", "application/json");
+                connectionPhoto.setRequestProperty("Authorization", "Bearer " + StartActivity.token);
+                connectionPhoto.connect();
+
+                InputStream isPhoto = connectionPhoto.getInputStream();
+                Bitmap b = BitmapFactory.decodeStream(isPhoto);
+
+                mProfile = new Profile(b, displayName, fhictId, studentId, classTitle, department, email);
+
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mDialog.setTitle("Loading");
+            mDialog.setMessage("Please wait...");
+            mDialog.show();
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            mDialog.dismiss();
+            Bitmap photo = mProfile.getPhoto();
+            Bitmap outputPhoto = Bitmap.createBitmap(photo.getWidth(),
+                    photo.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(outputPhoto);
+
+            final int color = 0xff424242;
+            final Paint paint = new Paint();
+            final Rect rect = new Rect(0, 0, photo.getWidth(), photo.getHeight());
+
+            paint.setAntiAlias(true);
+            canvas.drawARGB(0, 0, 0, 0);
+            paint.setColor(color);
+            // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+            canvas.drawCircle(photo.getWidth() / 2, photo.getHeight() / 2,
+                    photo.getWidth() / 2, paint);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            canvas.drawBitmap(photo, rect, rect, paint);
+            //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+
+
+            mImageView.setImageBitmap(outputPhoto);
+            mDisplayName.setText(mProfile.getDisplayName());
+            mFhictId.setText(mProfile.getFhictID());
+            mStudentId.setText(mProfile.getStudentID());
+            mClassTitle.setText(mProfile.getClassTitle());
+            mDepartment.setText(mProfile.getDepartment());
+            mEmail.setText(mProfile.getMail());
+        }
     }
 }
---
+++ app/src/main/java/com/example/ab/fhictcompanion/Model/Profile.java
@@ -0,0 +1,79 @@
+package com.example.ab.fhictcompanion.Model;
+
+import android.graphics.Bitmap;
+
+public class Profile {
+    private Bitmap photo;
+    private String displayName, fhictID, studentID, classTitle, department, mail;
+
+    public Profile(String fhictID, String studentID) {
+        this.fhictID = fhictID;
+        this.studentID = studentID;
+    }
+
+    public Profile(Bitmap photo, String displayName, String fhictID, String studentID, String classTitle, String department, String mail) {
+        this.photo = photo;
+        this.displayName = displayName;
+        this.fhictID = fhictID;
+        this.studentID = studentID;
+        this.classTitle = classTitle;
+        this.department = department;
+        this.mail = mail;
+    }
+
+    public Bitmap getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getFhictID() {
+        return fhictID;
+    }
+
+    public void setFhictID(String fhictID) {
+        this.fhictID = fhictID;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getClassTitle() {
+        return classTitle;
+    }
+
+    public void setClassTitle(String classTitle) {
+        this.classTitle = classTitle;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
