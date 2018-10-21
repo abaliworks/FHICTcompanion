04c5ed314cd5008cf23ae3948637d48abaf0c446
ab <shakawaka@protonmail.com>
2018-10-21 23:24:14 +0200

    add location functionality

--- app/src/main/java/com/example/ab/fhictcompanion/HomeActivity.java
+++ app/src/main/java/com/example/ab/fhictcompanion/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.ab.fhictcompanion;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -9,7 +10,7 @@ import android.view.View;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private CardView mSchedule, mPeople, mFontysNews, mAboutme;
+    private CardView mSchedule, mPeople, mLocation, mAboutme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +20,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         // Linking
         mSchedule = (CardView) findViewById(R.id.schedule);
         mPeople = (CardView) findViewById(R.id.people);
-        mFontysNews = (CardView) findViewById(R.id.news);
+        mLocation = (CardView) findViewById(R.id.location);
         mAboutme = (CardView) findViewById(R.id.aboutme);
         // Adding click listener
         mSchedule.setOnClickListener(this);
         mPeople.setOnClickListener(this);
-        mFontysNews.setOnClickListener(this);
+        mLocation.setOnClickListener(this);
         mAboutme.setOnClickListener(this);
     }
 
@@ -44,9 +45,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 i = new Intent(this,AboutMeActivity.class);
                 startActivity(i);
                 break;
-            case R.id.news:
-                i = new Intent(this,NewsActivity.class);
-                startActivity(i);
+            case R.id.location:
+                Uri geoIntengUri = Uri.parse("geo:51.452095,5.481963?q=" + Uri.encode("Fontys - Campus Rachelsmolen"));
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, geoIntengUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
                 break;
             default:
                 Log.d("error", "onClick: Nothing was CLICKKKEDDDD");
