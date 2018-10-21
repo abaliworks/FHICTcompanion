715864666204b2c15b669e37053fe49934d0cb02
ab <shakawaka@protonmail.com>
2018-10-21 13:38:46 +0200

    add Token debuging

--- app/src/main/java/com/example/ab/fhictcompanion/StartActivity.java
+++ app/src/main/java/com/example/ab/fhictcompanion/StartActivity.java
@@ -3,6 +3,7 @@ package com.example.ab.fhictcompanion;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartActivity extends AppCompatActivity implements TokenFragment.OnFragmentInteractionListener {
     public static String token;
@@ -15,6 +16,7 @@ public class StartActivity extends AppCompatActivity implements TokenFragment.On
     @Override
     public void onFragmentInteraction(String token) {
         this.token =token;
+        Log.d("YOOOOOOO", token);
         Intent intent = new Intent(StartActivity.this, MainActivity.class);
         intent.putExtra("token", token);
         startActivity(intent);
